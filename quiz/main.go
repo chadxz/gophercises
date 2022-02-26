8d8bc7cefeabdb5b6e892076731efb8267ae08cf
Chad McElligott <chad.mcelligott@gmail.com>
2022-02-25 23:40:20 -0600

    Add timer to quiz

--- quiz/main.go
+++ quiz/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 type problem struct {
@@ -18,30 +19,48 @@ type problem struct {
 func main() {
 	inputFileNamePtr := flag.String("input", "problems.csv",
 		"File to read questions and answers from")
-
+	timerDurationSecondsPtr := flag.Int("timer-seconds", 30,
+		"Seconds to wait before the quiz ends")
 	flag.Parse()
 
 	log.Print("Reading quiz from file: ", *inputFileNamePtr)
-	problems, err := readQuiz(*inputFileNamePtr)
+	problems, err := readProblemsFromCSV(*inputFileNamePtr)
 	check(err)
 
-	stdin := bufio.NewReader(os.Stdin)
-
 	correct := 0
+	timer := time.NewTimer(
+		time.Duration(*timerDurationSecondsPtr) * time.Second)
+	stdin := bufio.NewReader(os.Stdin)
 	for _, problem := range problems {
-		fmt.Printf("%s? ", problem.question)
-		answer, err := readAnswer(stdin)
-		check(err)
+		answerCh := make(chan string)
 
-		if answer == problem.answer {
-			correct++
+		fmt.Printf("%s? ", problem.question)
+		go requestAnswer(stdin, answerCh)
+
+		select {
+		case <-timer.C:
+			fmt.Printf("\ntime's up! %d/%d correct\n",
+				correct, len(problems))
+			return
+		case answer := <-answerCh:
+			if answer == problem.answer {
+				correct++
+			}
 		}
 	}
 
-	fmt.Printf("%d/%d correct\n", correct, len(problems))
+	fmt.Printf("all questions completed! %d/%d correct\n",
+		correct, len(problems))
 }
 
-func readQuiz(file string) ([]problem, error) {
+func requestAnswer(reader *bufio.Reader, resultCh chan<- string) {
+	answer, err := readAnswer(reader)
+	check(err)
+
+	resultCh <- answer
+}
+
+func readProblemsFromCSV(file string) ([]problem, error) {
 	questionsFile, err := os.Open(file)
 	if err != nil {
 		return []problem{}, err
