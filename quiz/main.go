b3e845576258916eabd8a6326a62a99bfa0e949f
Chad McElligott <chad.mcelligott@gmail.com>
2022-02-25 23:03:16 -0600

    Parse problems into struct

--- quiz/main.go
+++ quiz/main.go
@@ -11,50 +11,65 @@ import (
 )
 
 func main() {
-	questionsFileNamePtr := flag.String("input", "problems.csv",
+	inputFileNamePtr := flag.String("input", "problems.csv",
 		"File to read questions and answers from")
 
 	flag.Parse()
 
-	log.Print("Reading quiz from file: ", *questionsFileNamePtr)
-	records, err := readQuiz(*questionsFileNamePtr)
+	log.Print("Reading quiz from file: ", *inputFileNamePtr)
+	problems, err := readQuiz(*inputFileNamePtr)
 	check(err)
 
 	stdin := bufio.NewReader(os.Stdin)
-	results := struct {
-		correct int
-		total   int
-	}{}
 
-	for _, rec := range records {
-		fmt.Printf("%s? ", rec[0])
+	correct := 0
+	for _, problem := range problems {
+		fmt.Printf("%s? ", problem.question)
 		answer, err := readAnswer(stdin)
 		check(err)
 
-		results.total++
-		if answer == rec[1] {
-			results.correct++
+		if answer == problem.answer {
+			correct++
 		}
 	}
 
-	fmt.Printf("%d/%d correct\n", results.correct, results.total)
+	fmt.Printf("%d/%d correct\n", correct, len(problems))
 }
 
-func readQuiz(file string) ([][]string, error) {
+func readQuiz(file string) ([]problem, error) {
 	questionsFile, err := os.Open(file)
 	if err != nil {
-		return [][]string{}, err
+		return []problem{}, err
 	}
 	defer closeFile(questionsFile)
 
 	r := csv.NewReader(questionsFile)
 	records, err := r.ReadAll()
 	if err != nil {
-		return [][]string{}, err
+		return []problem{}, err
 	}
 
-	return records, nil
+	return parseProblems(records), nil
 }
+
+type problem struct {
+	question string
+	answer   string
+}
+
+func parseProblems(records [][]string) []problem {
+	results := make([]problem, len(records))
+
+	for i, record := range records {
+		results[i] = problem{
+			question: record[0],
+			answer:   strings.TrimSpace(record[1]),
+		}
+	}
+
+	return results
+}
+
 func readAnswer(reader *bufio.Reader) (string, error) {
 	result, err := reader.ReadString('\n')
 	if err != nil {
